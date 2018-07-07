6c5d27986ff68e59da8be673cad5a58403168647
Alexey Utkin <utkinalexey66@gmail.com>
2018-07-07 21:06:33 +0500

    Final
    удалил не нужные классы, сжал js css

---
+++ js/script1.js
@@ -0,0 +1,26 @@
+var link = document.querySelector(".button-popup");
+var popup = document.querySelector(".modal");
+var form = popup.querySelector("form");
+var indata = popup.querySelector("[name=indata]");
+var outdata = popup.querySelector("[name=outdata]");
+var adult = popup.querySelector("[name=adult]");
+var child = popup.querySelector("[name=child]");
+
+document.documentElement.classList.replace("no-js", "js");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  popup.classList.toggle("modal-show");
+
+  indata.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!indata.value || !outdata.value || !adult.value || !child.value) {
+    evt.preventDefault();
+     popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
