da3776f2c4893bc7f87bcf7dc019cd543d310226
Keks <keks@htmlacademy.ru>
2018-07-08 13:32:28 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/script.js
+++ 8/js/script.js
@@ -10,12 +10,17 @@ document.documentElement.classList.replace("no-js", "js");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
+
   popup.classList.toggle("modal-show");
+
+  indata.focus();
 });
 
 form.addEventListener("submit", function (evt) {
   if (!indata.value || !outdata.value || !adult.value || !child.value) {
     evt.preventDefault();
+     popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   }
 });
